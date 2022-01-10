501921ec458f35a0353e1af80ecc1c50fe8f6379
garry.willits <garry.willits@nhs.net>
2022-01-10 21:07:25 +0000

    gawi1-files for testing snyk

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
-FROM python:3-alpine
-WORKDIR /service
-COPY requirements.txt .
+FROM python:3.8.5-alpine3.12
+
+WORKDIR /app
+
+EXPOSE 5000
+ENV FLASK_APP=app.py
+
+COPY . /app
 RUN pip install -r requirements.txt
-COPY . ./
-EXPOSE 8080
-ENTRYPOINT ["python3", "app.py"]
+
+ENTRYPOINT [ "flask"]
+CMD [ "run", "--host", "0.0.0.0" ]
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
+# app.py
 import time
 from flask import Flask
+
 app = Flask(__name__)
 
 START = time.time()
-
 def elapsed():
     running = time.time() - START
     minutes, seconds = divmod(running, 60)
@@ -13,8 +14,9 @@ def elapsed():
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
 @app.route('/')
-def root():
+def home():
     return "Hello World (Python)! (up %s)\n" % elapsed()
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run()
