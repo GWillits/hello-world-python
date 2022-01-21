31522074698c2eaa6d31b29232c110ee10793592
Garry Willits <garry.willits@nhs.net>
2022-01-21 15:33:07 +0000

    4 attempt

--- scripts/safety-scan.sh
+++ scripts/safety-scan.sh
@@ -1,18 +1,20 @@
 fail_action="${1:-"FAIL"}"
-workspace="${2:-"."}"
+workspace="${2:-""}"
 user="${3:-"$(git log -n 1 --pretty=format:%an)"}"
 email="${4:-"$(git log -n 1 --pretty=format:%ae)"}"
 check_date=${5:-"$(date +'%m/%d/%Y : %X')"}
 check_date_str=${6:-"$(date +'%m.%d.%Y-%H.%M.%S')"}
 num_results=$(echo -n "$(safety check -r requirements.txt --bare)"| wc -w)
-artifact_path="$workspace/.github-artifacts"
 mkdir -p $artifact_path
 if [ $(($num_results)) -gt 0 ]; then
     echo  "$num_results package vulnerabilities discovered. Failing package scan test. Full report:"
     safety check -r requirements.txt --full-report
     # create report
     report_name="insecure_report_${check_date_str}"
-    jq   --null-input --arg DATE "$check_date" --arg USER "$user" --arg EMAIL "$email" '{origin:{usr: $USER,email:$EMAIL,date: $DATE}} ' | jq --argjson report "$(safety check -r requirements.txt --full-report --json)" '. + {report:$report}' > "${artifact_path}/${report_name}.json"
+    if  [ ! -z "${workspace}"]; then
+        artifact_path="$workspace/.github-artifacts"
+        jq   --null-input --arg DATE "$check_date" --arg USER "$user" --arg EMAIL "$email" '{origin:{usr: $USER,email:$EMAIL,date: $DATE}} ' | jq --argjson report "$(safety check -r requirements.txt --full-report --json)" '. + {report:$report}' > "${artifact_path}/${report_name}.json"
+    fi
     if [ "$fail_action" = "FAIL" ]
     then
         exit 1
