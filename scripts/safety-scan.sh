bdf6fbaaaf80e0dbc61c0e90e3965f4cb67cf5f2
Garry Willits <garry.willits@nhs.net>
2022-01-21 15:41:22 +0000

    final

--- scripts/safety-scan.sh
+++ scripts/safety-scan.sh
@@ -5,7 +5,7 @@ email="${4:-"$(git log -n 1 --pretty=format:%ae)"}"
 check_date=${5:-"$(date +'%m/%d/%Y : %X')"}
 check_date_str=${6:-"$(date +'%m.%d.%Y-%H.%M.%S')"}
 num_results=$(echo -n "$(safety check -r requirements.txt --bare)"| wc -w)
-mkdir -p $artifact_path
+
 if [ $(($num_results)) -gt 0 ]; then
     echo  "$num_results package vulnerabilities discovered. Failing package scan test. Full report:"
     safety check -r requirements.txt --full-report
@@ -13,6 +13,7 @@ if [ $(($num_results)) -gt 0 ]; then
     report_name="insecure_report_${check_date_str}"
     if  [ ! -z "${workspace}" ]; then
         artifact_path="$workspace/.github-artifacts"
+        mkdir -p $artifact_path
         jq   --null-input --arg DATE "$check_date" --arg USER "$user" --arg EMAIL "$email" '{origin:{usr: $USER,email:$EMAIL,date: $DATE}} ' | jq --argjson report "$(safety check -r requirements.txt --full-report --json)" '. + {report:$report}' > "${artifact_path}/${report_name}.json"
     fi
     if [ "$fail_action" = "FAIL" ]
