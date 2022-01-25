515b797372d9bcb639fee74539510d1459c94274
Garry Willits <garry.willits@nhs.net>
2022-01-25 11:47:57 +0000

    quick test

--- Makefile
+++ Makefile
@@ -6,10 +6,8 @@ install-poetry:
 	python3 -m pip install --user poetry
 
 install-ci:
-	
 	poetry install --no-dev --remove-untracked
-	. .venv/bin/activate
-	poetry run pip list
+
 
 map-requirements:
 	@if [ ! -f requirements.txt  ]; then poetry export -f requirements.txt --output requirements.txt; fi;
@@ -17,7 +15,7 @@ map-requirements:
 safety: map-requirements
 	@./scripts/safety-scan.sh ${safety_action}
 
-safety-ci: map-requirements
+safety-ci: map-py-requirements
 	@pip install safety
-	@./scripts/safety-scan.sh ${safety_action} ${workspace}
+	@./scripts/safety-scan.sh ${safety_action} ${workspace} ${detail} ${publish_artifacts}
 
--- scripts/safety-scan.sh
+++ scripts/safety-scan.sh
@@ -1,24 +1,42 @@
-fail_action="${1:-"FAIL"}"
-workspace="${2:-""}"
-user="${3:-"$(git log -n 1 --pretty=format:%an)"}"
-email="${4:-"$(git log -n 1 --pretty=format:%ae)"}"
-check_date=${5:-"$(date +'%m/%d/%Y : %X')"}
-check_date_str=${6:-"$(date +'%m.%d.%Y-%H.%M.%S')"}
-num_results=$(echo -n "$(safety check -r requirements.txt --bare)"| wc -w)
+#!/usr/bin/env bash
+# shellcheck disable=SC2034,SC2086
 
-if [ $(($num_results)) -gt 0 ]; then
+#vars
+fail_action="${1:-"FAIL"}"
+workspace="${2:-"."}"
+detail="${3:-"root-"}"
+publish_artifacts="${4:-""}"
+user="${5:-"$(git log -n 1 --pretty=format:%an)"}"
+email="${6:-"$(git log -n 1 --pretty=format:%ae)"}"
+check_date=${7:-"$(date +'%m/%d/%Y : %X')"}
+check_date_str=${8:-"$(date +'%m.%d.%Y-%H.%M.%S')"}
+# parse out those entries we want to ignore
+ignores=""
+num_ignored=0
+while read -r ignored_package || [[ -n $ignored_package ]]; do 
+    read -r ignored_code comment <<<"$(IFS="#"; echo "$ignored_package")"
+    ignores="$ignores -i $ignored_code"
+    ((num_ignored ++))
+done <"$workspace/ignored-safety.txt"
+num_results=$(safety check -r requirements.txt $ignores --bare | wc -w)
+
+if [ $num_results -gt 0 ]; then
     echo  "$num_results package vulnerabilities discovered. Failing package scan test. Full report:"
     safety check -r requirements.txt --full-report
     # create report
-    report_name="insecure_report_${check_date_str}"
-    if  [ ! -z "${workspace}" ]; then
+    report_name="${detail}-insecure_report_${check_date_str}"
+    if  [ "${publish_artifacts}" ]; then
+    echo "publishing artifact ${artifact_path}/${report_name}.json"
         artifact_path="$workspace/.github-artifacts"
-        mkdir -p $artifact_path
-        jq   --null-input --arg DATE "$check_date" --arg USER "$user" --arg EMAIL "$email" '{origin:{usr: $USER,email:$EMAIL,date: $DATE}} ' | jq --argjson report "$(safety check -r requirements.txt --full-report --json)" '. + {report:$report}' > "${artifact_path}/${report_name}.json"
+        mkdir -p "$artifact_path"
+        jq   --null-input --arg DATE "$check_date" --arg USER "$user" --arg EMAIL "$email" '{origin:{usr: $USER,email:$EMAIL,date: $DATE}} ' \
+        | jq --argjson report "$(safety check -r requirements.txt --full-report --json)" '. + {report:$report}' > "${artifact_path}/${report_name}.json"
     fi
     if [ "$fail_action" = "FAIL" ]
     then
         exit 1
     fi
+    else 
+        echo "No new vulnerabilities found. $num_ignored package(s) ignored from global ignore file"
 fi
 exit 0
